71563edb6ac7f90944e78a6c6e387b8257e3f2f6
Crazyafk <gill.fmk@oundleschool.org.uk>
2021-11-15 10:34:13 +0000

    added admin

---
+++ admin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<head>
+    <title>Hell's Library</title>
+</head>
+<body>
+    Reinstall Tables:
+    <form action="install.php" method="POST">
+        Include Test Data:
+        <input type="radio" name="testdata" value="yes">Yes<br>
+        <input type="radio" name="testdata" value="no">No<br>
+        <input type="submit" value="reset tables">
+    </form>
+</body>
--- index.php
+++ index.php
@@ -7,4 +7,5 @@
         include_once("conn.php");
         echo("welcome to the internet you smelly little potato");
     ?>
+    <a href="admin.php">Super Secret Admin Link</a>
 </body>
