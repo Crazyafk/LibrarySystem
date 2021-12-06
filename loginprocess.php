2fbbd602246ebd27a32f58fb413fc8a4272bc343
Crazyafk <gill.fmk@oundleschool.org.uk>
2021-12-06 09:54:07 +0000

    fixed login

--- loginprocess.php
+++ loginprocess.php
@@ -5,6 +5,7 @@
 <body>
     <?php
         include_once("conn.php");
+        session_start(); 
         array_map("htmlspecialchars",$_POST);
 
         $stmt = $conn->prepare("SELECT * FROM TblUsers WHERE Username = :username;");
@@ -14,7 +15,7 @@
         while($row = $stmt->fetch(PDO::FETCH_ASSOC))
         {
             if($row['Password']==$_POST['Pword']){
-                $_SESSION['name']=$row["surname"]; 
+                $_SESSION['name']=$row["Username"]; 
                 header('Location: index.php');
             }else{
                 header('Location: login.php');
